56604fa7f7e9b2d276ab4b8ceafa38cf61d6c171
Waleed Ammar <waleed.gamer173@gmail.com>
2025-09-04 21:41:44 +0400

    1.3

---
+++ access.js
@@ -0,0 +1,11 @@
+const granted = sessionStorage.getItem("access") === "accessgranted24h";
+const grantedTime = sessionStorage.getItem("accessTime");
+const now = Date.now();
+
+const twentyFourHours = 24 * 60 * 60 * 1000;
+
+if (!granted || !grantedTime || now - grantedTime > twentyFourHours) {
+  sessionStorage.removeItem("access");
+  sessionStorage.removeItem("accessTime");
+  window.location.href = "login.html"; 
+}
