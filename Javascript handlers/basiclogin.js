abb75e624b4c7fa8a35cacc15333d5e00a2c5719
Maq897 <sonofsheik@gmail.com>
2025-09-05 15:59:27 +0400

    1.6.5
    removed redeclarings

--- Javascript handlers/basiclogin.js
+++ Javascript handlers/basiclogin.js
@@ -4,20 +4,17 @@ const gate = params.get("accessReq");
 if (gate) {
   console.log("Access needed for:", gate);
   if (gate == "gate1") {
-    gate1();
+    gate("Gate 1")
   }
   if (gate == "gate2") {
-    gate2();
+    gate("Gate 2")
+  
   }
 }
 
 let btn = document.getElementById("btn");
 
-function gate1() {
+function gate(name) {
   const inp = document.getElementById("inp");
-  inp.innerHTML = `<label for="pinInput">Gate 1 </label>`;
-}
-function gate2() {
-  const inp = document.getElementById("inp");
-  inp.innerHTML = `<label for="pinInput">Gate 2 </label>`;
+  inp.innerHTML = `<label for="pinInput"> ${name} </label>`;
 }
