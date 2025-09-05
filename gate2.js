84a46cde85494e7f998d2a056df49d70fc01ebea
Waleed Ammar <waleed.gamer173@gmail.com>
2025-09-05 18:39:16 +0400

    3.1

--- gate2.js
+++ gate2.js
@@ -1,31 +1,31 @@
-// let admin = false;
-// let typed = "";
+let admin = false;
+let typed = "";
 
-// document.addEventListener('keydown', function(e) {
-//     if (e.key.length === 1) {
-//         typed += e.key;
-//         if (typed.toLowerCase().includes("admin")) {
-//             admin = true;
-//             alert("Admin mode enabled");
-//         }
-//     }
+document.addEventListener('keydown', function(e) {
+    if (e.key.length === 1) {
+        typed += e.key;
+        if (typed.toLowerCase().includes("admin")) {
+            admin = true;
+            alert("Admin mode enabled");
+        }
+    }
     
-//     if (!admin) {
-//         if (e.key === "F12") {
-//             e.preventDefault();
-//             return false;
-//         }
-//         // Ctrl+Shift+I, Ctrl+Shift+C, Ctrl+Shift+J
-//         if ((e.ctrlKey && e.shiftKey) && 
-//             (e.key === "I" || e.key === "C" || e.key === "J")) {
-//             e.preventDefault();
-//             window.location.href = "https://www.google.com/search?q=You+have+been+kicked";
-//             return false;
-//         }
-//     }
-// });
-// document.addEventListener('contextmenu', e => e.preventDefault());
-// document.addEventListener('selectstart', e => e.preventDefault());
+    if (!admin) {
+        if (e.key === "F12") {
+            e.preventDefault();
+            return false;
+        }
+        // Ctrl+Shift+I, Ctrl+Shift+C, Ctrl+Shift+J
+        if ((e.ctrlKey && e.shiftKey) && 
+            (e.key === "I" || e.key === "C" || e.key === "J")) {
+            e.preventDefault();
+            window.location.href = "https://www.google.com/search?q=You+have+been+kicked";
+            return false;
+        }
+    }
+});
+document.addEventListener('contextmenu', e => e.preventDefault());
+document.addEventListener('selectstart', e => e.preventDefault());
 
 document.addEventListener('keydown', event => {
   if ( event.key.toLowerCase() === "h") {
