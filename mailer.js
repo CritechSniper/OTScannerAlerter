3750b5a990d6f184dfdf7041be7fd9a04d482119
Maq897 <sonofsheik@gmail.com>
2026-09-10 21:14:26 +0400

    d

--- mailer.js
+++ mailer.js
@@ -32,6 +32,8 @@ export class Mailer {
   }
 }
 console.log('Loaded Mailer Script')
+const m = new Mailer()
+await m.mail("1097@iischoolabudhabi.com","t","t")
 /*
 ///---Still in work... do NOT UNCOMMENT \\\
 export class OTSDB {
